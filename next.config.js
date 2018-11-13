1e4e86a95eed8f777973d9061bc0ac294a76fb02
zypeh <zypeh.geek@gmail.com>
2018-11-13 16:51:41 +0800

    initial commit

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/**
+ * This file is not going through babel transformation.
+ */
+const debug = process.env.NODE_ENV !== 'production'
+const withMDX = require('@zeit/next-mdx')({
+  options: {
+    mdPlugins: [
+      require('remark-images'),
+      require('remark-emoji')
+    ]
+  }
+})
+
+module.exports = withMDX({
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' },
+    }
+  },
+  pageExtensions: ['js', 'jsx', 'mdx'],
+  assetPrefix: !debug ? '/site' : '',
+})
---
+++ pages/index.js
@@ -0,0 +1,2 @@
+import Document from './hello.mdx'
+export default () => <Document />
