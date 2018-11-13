0e7b91df79581707ae8e3f490d9a5e5fce9274ed
zypeh <zypeh.geek@gmail.com>
2018-11-13 18:16:38 +0800

    HTML headers for SEO

--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,15 @@
 import Document from './hello.mdx'
-export default () => <Document />
+import { Helmet } from "react-helmet"
+
+export default () => (
+    <main>
+        <Helmet>
+            <title>767D</title>
+            <meta
+                name="description"
+                content="Next generation web log for 767D."
+            />
+        </Helmet>
+        <Document />
+    </main>
+)
